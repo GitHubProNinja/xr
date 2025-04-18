3c973492cf25e8aa036e0e5813dfbc374d0a126e
GitHubProNinja <amirghodrat@gmail.com>
2025-04-17 22:11:13 -0400

    test

--- 02/js/script.js
+++ 02/js/script.js
@@ -68,7 +68,7 @@ function init() {
     window.addEventListener('resize', onWindowResize);
 
     cameraHolder = new THREE.Object3D();
-    // cameraHolder.position.z = 5;
+    cameraHolder.position.z = 5;
     // cameraHolder.position.y = 2;
     cameraHolder.add(camera);
     scene.add(cameraHolder);
