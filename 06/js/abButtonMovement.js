08d90e4a840f3c08214e16cd0bd9a1d33963b335
GitHubProNinja <amirghodrat@gmail.com>
2025-04-21 00:54:54 -0400

    test

--- 06/js/abButtonMovement.js
+++ 06/js/abButtonMovement.js
@@ -5,9 +5,9 @@ export function setupABButtonMovement(renderer, scene, cameraHolder) {
     const controllerModelFactory = new XRControllerModelFactory();
 
     // Right controller
-    rightController = renderer.xr.getController(0);
+    rightController = renderer.xr.getController(1);
     scene.add(rightController);
-    rightControllerGrip = renderer.xr.getControllerGrip(0);
+    rightControllerGrip = renderer.xr.getControllerGrip(1);
     rightControllerGrip.add(controllerModelFactory.createControllerModel(rightControllerGrip));
     scene.add(rightControllerGrip);
 
