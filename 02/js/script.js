d3e0ab3942c3723c7956ee2e967085cd58c2eb2e
GitHubProNinja <amirghodrat@gmail.com>
2025-04-21 00:37:49 -0400

    test

--- 02/js/script.js
+++ 02/js/script.js
@@ -2,13 +2,14 @@ import * as THREE from 'three';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import Stats from 'three/addons/libs/stats.module.js';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
-import { setupMovementControllers } from './movementControls.js';
+import { handleABButtonMovement } from './abButtonMovement.js';
 
 let scene, renderer, camera, stats;
 let dirLight, dirLightHelper;
 let gridHelper, axesHelper;
 let ground, ambientLight;
 let cameraHolder;
+let rightController;
 
 init();
 
@@ -70,12 +71,15 @@ function init() {
     cameraHolder.add(camera);
     scene.add(cameraHolder);
 
+    rightController = renderer.xr.getController(0);
+    scene.add(rightController);
+
     createGUI();
-    setupMovementControllers(renderer, scene, cameraHolder);
     renderer.setAnimationLoop(animate);
 }
 
 function animate() {
+    handleABButtonMovement(rightController, cameraHolder);
     renderer.render(scene, camera);
     stats.update();
 
